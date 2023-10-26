8b451343b2ba3b4ffe1cd11d7792ad4dee16feea
Marian Luca <marianluca404@gmail.com>
2023-10-26 23:52:58 +0200

    Design

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
@@ -9,6 +8,10 @@ import javax.swing.*;
 
 /**
  * This class creates the maze game itself.
+ * It creates a JFrame with two JPanels.
+ * The first JPanel displays the map and the character sprite.
+ * The second JPanel contains a JButton for restarting the level
+ * and a timer for the player to know how much time he has.
  */
 
 public class GameWindow {
@@ -26,34 +29,49 @@ public class GameWindow {
     int currentLevel;
    
     
-
+    /**
+     * The constructor of the class.
+     * It initiates all the instance variables of the Swing class. 
+     */
     public GameWindow() {
         window = new JFrame("Maze Game");
-        
         panel2 = new JPanel();
-        restartButton = new JButton("Restart Level");
+        restartButton = new JButton("Restart");
         timeLabel = new JLabel("Start");
     }
 
+    /**
+     * This method is responsible for initiating the actual game window.
+     * It creates an instance of the Maze class which will display the 
+     * maze and the character sprite in the first JPanel (the Maze class extends the JPanel class)
+     * and displays a JPanel with a JButton and a JLabel.
+     * "restartButton" can be used to restart the current level of the game
+     * by calling the restartLevel() method.
+     * timeLabel displays the timer of the current level to let the player know how much time he has
+     * left by calling the countdownTimer() method.
+     */
     public void mazeGame(int level) {
 
         maze = new Maze(level * 5, this);
         maze.startGameThread();
         currentLevel = level;
         restartButton.setFocusable(false);
+        restartButton.setFont(new Font("Monospace", Font.BOLD, 20));
+        restartButton.setBackground(new Color(0x449c5c));
+        restartButton.setForeground(Color.white);
         panel2.setPreferredSize(new Dimension(maze.mazeLength, 50));
+        panel2.setBackground(new Color(0x449c5c));
         panel2.setLayout(new GridLayout(1, 2));
         panel2.add(restartButton);
         timeLabel.setHorizontalAlignment(JLabel.CENTER);
+        timeLabel.setFont(new Font("Monospace", Font.BOLD, 20));
+        timeLabel.setForeground(Color.white);
         panel2.add(timeLabel);
         window.add(maze, BorderLayout.NORTH);
         window.add(panel2, BorderLayout.SOUTH);
 
-
-       // window.setPreferredSize(new Dimension(maze.mazeLength, maze.mazeLength + 100));
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setFocusable(false);
-        
         window.setResizable(false);
         window.pack();
         window.setLocationRelativeTo(null);
@@ -77,7 +95,12 @@ public class GameWindow {
     }
 
     
-
+    /**
+     * This method restarts the current level of the game when
+     * "restartButton" is pressed.
+     * It does that by creating a new instance of the GameWindow class and disposing 
+     * of the old one.
+     */
     public void restartLevel() {
         //if restart button clicked restart level
         window.dispose();
@@ -85,8 +108,20 @@ public class GameWindow {
 
     }
 
+    /**
+     * This method creates the countdown timer for the current level.
+     * It does that by using a timer and every second it decrements the
+     * second variable and it displays the new text in a 00:00 format.
+     * When "second" is smaller than zero, meaning a minute has passed, the 
+     * "minute" variable decrements and "second" is set to 59, in order to properly
+     * simulate the working of a timer.
+     * Finally, if the timer hits 00:00, meaning both "minute" and "second" are zero,
+     * the game ends, the gameThiread from the Maze class is set to null, thus game
+     * panel (the first panel) stops being repainted and the player cannot move anymore.
+     * A losw window (JFrame) is displayed by calling the loseWindow() method
+     */
     public void countdownTimer() {
-        //displays timeLabel
+        
         timer = new Timer(1000, new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 second--;
@@ -110,6 +145,7 @@ public class GameWindow {
         });
     }
 
+    
     public void winWindow() {
         //creates win frame with 2 buttons
         //press menu to go to levels menu
@@ -131,22 +167,37 @@ public class GameWindow {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(1, 2));
         JButton menu = new JButton("Menu");
+        menu.setBackground(new Color(0x449c5c));
+        menu.setForeground(Color.white);
+        menu.setFont(new Font("Monospace", Font.BOLD, 20));
+        menu.setFocusable(false);
         JButton nextLevel = new JButton("Next Level");
-        JLabel label1 = new JLabel("YOU WON");
-        label1.setHorizontalAlignment(JLabel.CENTER);
-        JLabel label2 = new JLabel("You Completed the Game");
+        nextLevel.setBackground(new Color(0x449c5c));
+        nextLevel.setForeground(Color.white);
+        nextLevel.setFont(new Font("Monospace", Font.BOLD, 20));
+        nextLevel.setFocusable(false);
+        JLabel winMessage = new JLabel("YOU WON!");
+        winMessage.setHorizontalAlignment(JLabel.CENTER);
+        winMessage.setForeground(new Color(0xffc20e));
+        winMessage.setFont(new Font("Monospace", Font.BOLD, 40));
+        JLabel label2 = new JLabel("You Completed the Game!");
         label2.setHorizontalAlignment(JLabel.CENTER);
+        label2.setForeground(new Color(0xffc20e));
+        label2.setFont(new Font("Monospace", Font.BOLD, 30));
 
         if (currentLevel == 3) {
             winFrame.add(label2);
+            menu.setPreferredSize(new Dimension(300, 50));
             winFrame.add(menu, BorderLayout.SOUTH);
         } else {
+            panel.setPreferredSize(new Dimension(300, 50));
             panel.add(menu);
             panel.add(nextLevel);
-            winFrame.add(label1);
+            winFrame.add(winMessage);
             winFrame.add(panel, BorderLayout.SOUTH);
         }
 
+        winFrame.getContentPane().setBackground(new Color(0x449c5c));
         winFrame.setPreferredSize(new Dimension(300, 400));
         winFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         winFrame.pack();
@@ -182,16 +233,29 @@ public class GameWindow {
         //retry and back to level menu
         JFrame loseFrame = new JFrame("Maze Game");
         JPanel pan = new JPanel();
-        JLabel loseMessage = new JLabel("YOU LOST");
-        JButton retry = new JButton("Retry");
-        JButton menu = new JButton("Menu");
+        JLabel loseMessage = new JLabel("YOU LOST!");
+        loseMessage.setForeground(new Color(0xffc20e));
+        loseMessage.setFont(new Font("Monospace", Font.BOLD, 40));
         loseMessage.setHorizontalAlignment(JLabel.CENTER);
+        JButton retry = new JButton("Retry");
+        retry.setBackground(new Color(0xd1242c));
+        retry.setForeground(Color.white);
+        retry.setFont(new Font("Monospace", Font.BOLD, 20));
+        retry.setFocusable(false);
+        JButton menu = new JButton("Menu");
+        menu.setBackground(new Color(0xd1242c));
+        menu.setForeground(Color.white);
+        menu.setFont(new Font("Monospace", Font.BOLD, 20));
+        menu.setFocusable(false);
+        pan.setPreferredSize(new Dimension(300, 50));
         pan.setLayout(new GridLayout(1, 2));
         pan.add(retry);
         pan.add(menu);
         loseFrame.add(loseMessage);
         loseFrame.add(pan, BorderLayout.SOUTH);
 
+
+        loseFrame.getContentPane().setBackground(new Color(0xd1242c));
         loseFrame.setPreferredSize(new Dimension(300, 400));
         loseFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         loseFrame.pack();
--- src/StartMenu.java
+++ src/StartMenu.java
@@ -4,52 +4,75 @@ import javax.swing.*;
 
 /**
  * This class initiates the game and creates the Start menu.
+ * The Start menu consists of a JLabel that makes up the title 
+ * of the game and a panel with 2 buttons.
+ * The "Play Game" button loads up the levels' menu
+ * The "Instructions" button creates a new JFrame which contains a JLabel
+ * with the rules of the game.
  */
 
 public class StartMenu  {
     JFrame startMenu;
     JPanel panel;
+    JLabel title;
     JButton playButton;
     JButton instructionButton;
     JLabel instructions;
+    ImageIcon titleName;
     
     /**
      * The constructor of the class.
+     * It initializez all of the instance variables.
      */
     public StartMenu() {
         startMenu = new JFrame("Maze Game");
         panel = new JPanel();
+        titleName = new ImageIcon(getClass().getResource("/Character/title.png"));
+        title = new JLabel();
         playButton = new JButton("Play Game");
         instructionButton = new JButton("Instructions");
     
     }
 
     /**
-    * The method used to initiate the class and start the game.
+     * This method is used to initialize the class and creates the Start menu.
+     * It creates a JFrame with GridLayout as it's layout manager.
+     * It contains a JLabel with the title image and a JPanel with two JButtons.
+     * playButton loads up the levels' menu by making an instance of the Levels class.
+     * instructionButton creates the instructions window by calling the instructMenu method.
     */
 
     public void startGame() {
-        panel.setPreferredSize(new Dimension(500, 500));
-        panel.setLayout(new GridLayout(3, 1));
-        panel.setBackground(Color.black);
-        //startMenu.setLayout(new GridLayout(2, 1));
+        
+        panel.setLayout(new GridLayout(2, 1));
+        panel.setPreferredSize(new Dimension(500, 100));
+        panel.setBackground(new Color(0x449c5c));
+        title.setIcon(titleName);
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setVerticalAlignment(JLabel.CENTER);
+        title.setForeground(Color.white);
         playButton.setFocusable(false);
         playButton.setPreferredSize(new Dimension(500, 50));
-        playButton.setBackground(Color.black);
+        playButton.setBackground(new Color(0x449c5c));
         playButton.setForeground(Color.white);
-        playButton.setFont(new Font("Monospace", Font.BOLD, 30));
+        playButton.setFont(new Font("Monospace", Font.BOLD, 25));
         instructionButton.setFocusable(false);
-        instructionButton.setBackground(Color.black);
+        instructionButton.setBackground(new Color(0x449c5c));
         instructionButton.setForeground(Color.white);
-        instructionButton.setFont(new Font("Monospace", Font.BOLD, 30));
+        instructionButton.setFont(new Font("Monospace", Font.BOLD, 25));
         instructionButton.setPreferredSize(new Dimension(500, 50));
+        
         panel.add(playButton);
         panel.add(instructionButton, BorderLayout.SOUTH);
+        startMenu.setLayout(new GridLayout(2, 1));
+        startMenu.setPreferredSize(new Dimension(500, 500));
+        startMenu.getContentPane().setBackground(new Color(0x449c5c));
+        startMenu.add(title);
         startMenu.add(panel);
 
         instructions = new JLabel("" + "<html>"
         + "1. Collect all the coins and solve the maze before the timer ends<br>"
-        + "2. You must reach the red square in order to solve the maze<br>"
+        + "2. You must reach the trophy in order to solve the maze<br>"
         + "3. You cannot finish the level before collecting all coins<br>"
         + "4. Use the W, A, S, D keys to move your character<br>"
         + "Have Fun!!!</html>");
@@ -78,10 +101,16 @@ public class StartMenu  {
     }
     
     /**
-     * This method creates the instruction window.
+     * This method creates the instructions window.
+     * It creates a new JFrame that contains the rules of the game.
+     * It's default close operation is purposefully set to HIDE_ON_CLOSE
+     * so that the the game doesn't terminate when the user closes it.
      */
     void instructMenu() {
         JFrame frame = new JFrame("Instructions");
+        instructions.setHorizontalAlignment(JLabel.CENTER);
+        
+        instructions.setFont(new Font("Monospace", Font.BOLD, 20));
         frame.getContentPane().add(instructions, BorderLayout.CENTER);
         frame.setPreferredSize(new Dimension(400, 300));
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
